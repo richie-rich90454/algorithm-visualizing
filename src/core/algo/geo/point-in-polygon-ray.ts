6c5adacd759e49e4aebdcbd54250af279091197f
richie-rich90454 <rjiang880@gmail.com>
2026-08-04 15:11:58 +0800

    fix: use American English spelling in point-in-polygon-ray.ts

--- src/core/algo/geo/point-in-polygon-ray.ts
+++ src/core/algo/geo/point-in-polygon-ray.ts
@@ -16,7 +16,7 @@
  *   Space: O(1)
  *
  * ---------------------------------------------------------------------------
- * Visualisation mapping
+ * Visualization mapping
  * ---------------------------------------------------------------------------
  *   - The polygon is drawn as a closed polyline.
  *   - The query point is YELLOW (comparing).
